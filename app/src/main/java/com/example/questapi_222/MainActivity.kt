4fd630180bddfda65fd22255248820aed128078e
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-18 20:47:55 +0700

    setup MainActivity dan inisialisasi DataSiswaApp

---
+++ app/src/main/java/com/example/questapi_222/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.questapi_222
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questapi_222.ui.theme.QuestAPI_222Theme
+import com.example.questapi_222.uicontroller.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            QuestAPI_222Theme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
