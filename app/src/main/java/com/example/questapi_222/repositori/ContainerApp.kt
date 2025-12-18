f689af38f73c843ad89b033815ffd9b26dc36389
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-18 19:03:30 +0700

    menambahkan interface ContainerApp untuk dependency injection

---
+++ app/src/main/java/com/example/questapi_222/repositori/ContainerApp.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_222.repositori
+
+
+// 1. Interface Container untuk Dependency Injection
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
+
