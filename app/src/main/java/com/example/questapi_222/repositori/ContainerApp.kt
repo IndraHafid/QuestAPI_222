e4bfc33be1333ea375c4ff1669dea8a9bd08641c
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-18 19:14:04 +0700

    implementasi DefaultContainerApp dengan RepositoryDataSiswa

--- app/src/main/java/com/example/questapi_222/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_222/repositori/ContainerApp.kt
@@ -1,5 +1,6 @@
 package com.example.questapi_222.repositori
 
+import com.example.questapi_222.apiservice.ServiceApiSiswa
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -39,4 +40,15 @@ class DefaultContainerApp : ContainerApp {
         )
         .client(klien)
         .build()
+
+    // Inisialisasi Service API secara Lazy
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    // Implementasi Repository yang akan digunakan di ViewModel
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(retrofitService)
+    }
+}
 
