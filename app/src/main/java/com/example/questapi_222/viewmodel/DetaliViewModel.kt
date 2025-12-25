0fbb69eb827d3f2405588943a9a4202c7812b8d0
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-25 17:00:16 +0700

    implementasi fetch satu siswa dengan handling error

--- app/src/main/java/com/example/questapi_222/viewmodel/DetaliViewModel.kt
+++ app/src/main/java/com/example/questapi_222/viewmodel/DetaliViewModel.kt
@@ -6,10 +6,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_222.modeldata.DataSiswa
 import com.example.questapi_222.repositori.RepositoryDataSiswa
 import com.example.questapi_222.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
 import kotlinx.serialization.InternalSerializationApi
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed interface StatusUIDetail {
     data class Success(val siswa: DataSiswa) : StatusUIDetail
@@ -27,3 +31,22 @@ RepositoryDataSiswa):ViewModel() {
     init {
         getSatuSiswa()
     }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(
+                    siswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                )
+            }
+            catch (e: IOException){
+                StatusUIDetail.Error
+            }
+            catch (e: HttpException){
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+}
