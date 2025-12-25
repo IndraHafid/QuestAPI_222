68b9bfd916ccdc6a4e29fbd713943ed11ded3548
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-25 16:26:09 +0700

    menambahkan FloatingActionButton untuk navigasi edit data siswa

--- app/src/main/java/com/example/questapi_222/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_222/view/HalamanDetail.kt
@@ -1,18 +1,28 @@
 package com.example.questapi_222.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_222.R
 import com.example.questapi_222.uicontroller.route.DestinasiDetail
 import com.example.questapi_222.viewmodel.DetailViewModel
+import com.example.questapi_222.viewmodel.StatusUIDetail
 import com.example.questapi_222.viewmodel.provider.PenyediaViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
@@ -25,7 +35,27 @@ fun DetailSiswaScreen(
                 navigateUp = navigateBack
             )
         },
-
+        floatingActionButton = {
+            val uiState = viewModel.statusUIDetail
+            FloatingActionButton(
+                onClick = {
+                    when (uiState) {
+                        is StatusUIDetail.Success ->
+                            navigateToEditItem(uiState.siswa.id)
+                        else -> {}
+                    }
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(
+                    dimensionResource(id = R.dimen.padding_large)
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update)
+                )
+            }
+        },
         modifier = modifier
     ) {
 }
