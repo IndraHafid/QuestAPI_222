edc4a2073e87d7f9b4e969d2571c00f59767225a
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-25 16:56:15 +0700

    implementasi update data siswa ke server

--- app/src/main/java/com/example/questapi_222/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_222/viewmodel/EditViewModel.kt
@@ -8,10 +8,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questapi_222.modeldata.DetailSiswa
 import com.example.questapi_222.modeldata.UIStateSiswa
+import com.example.questapi_222.modeldata.toDataSiswa
 import com.example.questapi_222.modeldata.toUiStateSiswa
 import com.example.questapi_222.repositori.RepositoryDataSiswa
 import com.example.questapi_222.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
+import retrofit2.Response
+
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
 RepositoryDataSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
@@ -36,4 +39,16 @@ RepositoryDataSiswa): ViewModel() {
         }
     }
 
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa, uiStateSiswa
+                .detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful){
+                println("Update Sukses : ${call.message()}")
+            }else{
+                println("Update Error : ${call.errorBody()}")
+            }
+        }
+    }
 }
